715d8377abf60a31d4e9e7d3f7e0d570d3537c0c
Bastien <bmoulia@gmail.com>
2017-08-21 21:13:11 +0200

    Add exercise in calcul of insulin
    Fix #12

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,12 @@ import { ParametersPage } from '../parameters/parameters';
 import { BloodGlucoseService } from '../../app/blood-glucose.service';
 import { PhysiologicalDataService } from '../../app/physiological-data.service';
 
+interface PhysicalActivityStep {
+  value: number;
+  label: string;
+  reduction: number;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -12,7 +18,29 @@ import { PhysiologicalDataService } from '../../app/physiological-data.service';
 export class HomePage {
   glucose: number;
   insulinUnit: number;
+  physicalActivity: number;
+  physicalActivities: PhysicalActivityStep[]
   constructor(public navCtrl: NavController, public bloodGlucoseService: BloodGlucoseService, public physiologicalDataService: PhysiologicalDataService) {
+    this.physicalActivity = 0;
+    this.physicalActivities = [
+      {
+        value: 0,
+        label: 'none',
+        reduction: 0
+      }, {
+        value: 1,
+        label: 'light',
+        reduction: 20
+      }, {
+        value: 2,
+        label: 'medium',
+        reduction: 35
+      }, {
+        value: 3,
+        label: 'intense',
+        reduction: 50
+      }, 
+    ]
   }
 
   calcul() {
@@ -24,6 +52,7 @@ export class HomePage {
       } else {
         this.insulinUnit = 0;
       }
+      this.insulinUnit = this.insulinUnit - this.insulinUnit * this.physicalActivities[this.physicalActivity].reduction / 100;
     } else {
       this.insulinUnit = null;
     }
