5dd9ce6c0c4cc781d0e0004a303ebd32fd707e0f
Bastien <bmoulia@gmail.com>
2017-08-28 22:54:32 +0200

    Add translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, Config } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TranslateService } from '@ngx-translate/core';
@@ -12,7 +12,7 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService, config: Config) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -22,6 +22,9 @@ export class MyApp {
       let userLang = navigator.language.split('-')[0];
       userLang = /(en|fr)/gi.test(userLang) ? userLang : 'en';
       translate.use(userLang);
+      translate.get('BACK').subscribe((back: string) => {
+        config.set('ios','backButtonText', back)
+      });
     });
   }
 }
